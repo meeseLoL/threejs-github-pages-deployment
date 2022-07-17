394161c06274bee63929700819f661b47a2374b7
meeseLoL <107075158+meeseLoL@users.noreply.github.com>
2022-07-17 15:42:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.min.js';
 
-import { OrbitControls } from 'https://cdn.jsdelivr.net/combine/npm/three-orbitcontrols@2.110.3/example.min.js,npm/three-orbitcontrols@2.110.3,npm/three-orbitcontrols@2.110.3/test.min.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
